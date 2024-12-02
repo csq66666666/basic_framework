629d3aa80591d305f8259c9ab978f6fa96b783e6
Weedy-Powered <751127773@qq.com>
2024-12-02 16:03:22 +0800

    整车底盘部分
    修改了robot_def

--- application/chassis/chassis.c
+++ application/chassis/chassis.c
@@ -1,11 +1,11 @@
 /**
  * @file chassis.c
- * @author NeoZeng neozng1@hnu.edu.cn
+ * @author Weedy
  * @brief 底盘应用,负责接收robot_cmd的控制命令并根据命令进行运动学解算,得到输出
  *        注意底盘采取右手系,对于平面视图,底盘纵向运动的正前方为x正方向;横向运动的右侧为y正方向
  *
  * @version 0.1
- * @date 2022-12-04
+ * @date 2024-12-2
  *
  * @copyright Copyright (c) 2022
  *
